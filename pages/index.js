c70ecaa30b0919471035a246032e99af6968ac99
Ben <cooperben83@gmail.com>
2020-09-17 17:16:46 -0500

    phone zoom on text input focus bug fix attempt 1

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,10 @@ const Home = () => (
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,500;0,600;0,700;1,100;1,400;1,700&display=swap"
         rel="stylesheet"
       />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, maximum-scale=1.0, width=device-width"
+      />
     </Head>
     <div className="container mx-auto">
       <StickyNav />
