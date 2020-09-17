21c4ee0c16f0ece89a3196026c7c1a9d1346ee81
Ben <cooperben83@gmail.com>
2020-09-17 16:43:54 -0500

    changed how-it-works statement

--- src/components-mobile/howItWorks-mobile.jsx
+++ src/components-mobile/howItWorks-mobile.jsx
@@ -8,7 +8,9 @@ const HowItWorks = () => {
   return (
     <div className="container primary-bg primary-text w-full px-4 pr-10 mb-20 ">
       <p className="text-4xl font-bold">How it works</p>
-      <p className="text-xl font-regular">You vent & vote. We pass it on. </p>
+      <p className="text-xl font-regular">
+        You vocalize & vote; <br /> We pass it on.
+      </p>
       <Voice />
       <Amplify />
       <Connect />
