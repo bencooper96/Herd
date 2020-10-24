c089199fb475cbf15fa30b9e9394734612134ea5
Ben <cooperben83@gmail.com>
2020-10-23 23:14:17 -0500

    added padding & placement of text

--- src/components/howItWorks.jsx
+++ src/components/howItWorks.jsx
@@ -4,8 +4,8 @@ import HIWMobile from "./howItWorksContent/hiwMobileLayout"
 
 const HowItWorks = () => {
   return (
-    <div className="w-full sm:px-10 mt-20 bg-district bg-cover bg-no-repeat bg-fixed">
-      <p className="text-4xl md:text-5xl font-bold">How it works</p>
+    <div className="w-full sm:px-10 mt-20 bg-district bg-cover bg-no-repeat bg-fixed pt-2 sm:pt-0">
+      <p className="text-4xl md:text-5xl font-bold sm:text-left text-center">How it works</p>
       <HIWDesktop/>
       <HIWMobile/>
 
