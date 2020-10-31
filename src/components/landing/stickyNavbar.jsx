70bbeac2c1c9b938055b39d0220b99e757833865
Ben <cooperben83@gmail.com>
2020-10-31 10:10:29 -0500

    stickynav over all

--- src/components/landing/stickyNavbar.jsx
+++ src/components/landing/stickyNavbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function StickyNav() {
   return (
-    <div className="sticky top-0 z-0 h-10 w-full">
+    <div className="sticky top-0 z-40 h-10 w-full">
       <Link href="#landing">
         <button className=" float-right mx-5 my-2 w-1/4 md:w-1/6 text-xl call-to-action-btn">
           Sign up
